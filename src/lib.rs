c9634ac1854725b7407cf21b2c6a7fa9981133d4
Arunas <karunce@gmail.com>
2023-11-08 11:19:51 +0000

    refactor

--- src/lib.rs
+++ src/lib.rs
@@ -6,31 +6,17 @@ use nih_plug::prelude::*;
 use std::ops::DerefMut;
 use std::sync::Arc;
 
-#[derive(Clone, Default)]
-struct Buf {
-    read: f32,
-    write: usize,
+#[derive(Clone, Debug)]
+struct Channel {
     data: Vec<f32>,
+    write: usize,
+    read: f32,
+    recording: bool,
+    reverse_playback: bool,
+    playback_volume: f32,
+    passthru_volume: f32,
 }
-
-impl Buf {
-    pub fn new() -> Self {
-        Self {
-            read: 0.0,
-            write: 0,
-            data: vec![],
-        }
-    }
-    pub fn write(&mut self, value: f32) {
-        assert!(self.write <= self.data.len());
-        if self.write == self.data.len() {
-            self.data.push(value);
-        } else {
-            self.data[self.write] = value;
-        }
-        self.write += 1;
-    }
-
+impl Channel {
     pub fn calc_sample_pos(&self) -> usize {
         let len_f32 = (self.data.len() as f32);
         let i = self.read % len_f32;
@@ -38,115 +24,44 @@ impl Buf {
         let i = i as usize;
         i
     }
-
-    pub fn read(&mut self, speed: f32) -> f32 {
-        if self.data.is_empty() {
-            0.0
-        } else {
-            let i = self.calc_sample_pos();
-            self.read += speed;
-            self.data[i]
+}
+impl Default for Channel {
+    fn default() -> Self {
+        Channel {
+            data: Vec::new(),
+            write: 0,
+            read: 0.0,
+            recording: false,
+            reverse_playback: false,
+            playback_volume: 0.0,
+            passthru_volume: 1.0,
         }
     }
-
-    pub fn rewind_write(&mut self) {
-        self.write = 0;
-    }
-
-    pub fn truncate_written(&mut self) {
-        self.data.truncate(self.write);
-        self.write = 0;
-    }
-
-    pub fn clear(&mut self) {
-        self.data.clear();
-        self.read = 0.0;
-        self.write = 0;
-    }
-
-    pub fn rewind_read(&mut self) {
-        self.read = 0.0;
-    }
-
-    pub fn seek(&mut self, pos: f32) {
-        assert!(
-            pos >= 0.0 && pos <= 1.0,
-            "pos is not in range 0.0 1.0: {}",
-            pos
-        );
-        self.read = ((self.data.len() as f32) * pos);
-    }
 }
 
-#[derive(Clone, Default)]
-struct Bufs {
-    v: Vec<Buf>,
+#[derive(Clone, Default, Debug)]
+struct Channels {
+    channels: Vec<Channel>,
 }
 
-impl Bufs {
-    pub fn new(channel_count: usize) -> Self {
-        Bufs {
-            v: vec![Buf::new(); channel_count],
+impl Channels {
+    pub fn new(count: usize) -> Self {
+        Self {
+            channels: vec![Channel::default(); count],
         }
     }
-    pub fn len(&self) -> usize {
-        self.v.len()
+    pub fn each<F>(&mut self, f: F)
+    where
+        F: FnMut(&mut Channel),
+    {
+        self.channels.iter_mut().for_each(f)
     }
-    pub fn clear(&mut self) {
-        self.v.iter_mut().for_each(|b| b.clear());
-    }
-    pub fn rewind_read(&mut self) {
-        self.v.iter_mut().for_each(|b| b.rewind_read());
-    }
-    pub fn rewind_write(&mut self) {
-        self.v.iter_mut().for_each(|b| b.rewind_write());
-    }
-    #[cfg(debug_assertions)]
-    pub fn check_write_positions_at_zero(&self) {
-        assert!(self.v.iter().all(|b| b.write == 0))
-    }
-    pub fn truncate_written(&mut self) {
-        self.v.iter_mut().for_each(|b| b.truncate_written());
-    }
-    pub fn calc_sample_pos(&self) -> usize {
-        self.v[0].calc_sample_pos()
-    }
-    pub fn seek(&mut self, pos: f32) {
-        self.v.iter_mut().for_each(|b| b.seek(pos));
-    }
-    //pub fn ensure_channel_count(&mut self, count: usize) {
-    //    if count >= self.v.len() {
-    //        self.v.resize(count + 1, Buf::default());
-    //    }
-    //}
-    pub fn write(&mut self, channel: usize, value: f32) {
-        self.v[channel].write(value);
-    }
-    pub fn read(&mut self, channel: usize, speed: f32) -> f32 {
-        self.v[channel].read(speed)
-    }
-}
-
-#[derive(Clone, Debug, PartialEq)]
-struct Playing {
-    speed: f32,
-}
-
-#[derive(Clone, Default, Debug, PartialEq)]
-struct State {
-    recording: bool,
-    playing: Option<Playing>,
-    passthru: bool,
 }
 pub struct LiveSampler {
+    channels: Channels,
     audio_io_layout: AudioIOLayout,
     params: Arc<LiveSamplerParams>,
     sample_rate: f32,
-    buf: Bufs,
-    volume: PolyVolumeEnv,
-    passthru_volume: PolyVolumeEnv,
-    now: Vec<usize>,
-    state: State,
 }
 
 #[derive(Params)]
@@ -157,8 +72,8 @@ struct LiveSamplerParams {
     pub passthru: BoolParam,
     #[id = "speed"]
     pub speed: FloatParam,
-    #[id = "fade time"]
-    pub fade_time: FloatParam,
+    //#[id = "fade time"]
+    //pub fade_time: FloatParam,
 }
 
 impl Default for LiveSamplerParams {
@@ -186,17 +101,17 @@ impl Default for LiveSamplerParams {
                     max: 1.0,
                 },
             ),
-            fade_time: FloatParam::new(
-                "Fade time",
-                100.0,
-                FloatRange::Linear {
-                    min: 0.0,
-                    max: 1000.0,
-                },
-            )
-            .with_unit(" samples"), //with_smoother(SmoothingStyle::Logarithmic(50.0))
-                                    //.with_value_to_string(formatters::v2s_f32_gain_to_db(2))
-                                    //.with_string_to_value(formatters::s2v_f32_gain_to_db()),
+            //fade_time: FloatParam::new(
+            //    "Fade time",
+            //    100.0,
+            //    FloatRange::Linear {
+            //        min: 0.0,
+            //        max: 1000.0,
+            //    },
+            //)
+            //.with_unit(" samples"), //with_smoother(SmoothingStyle::Logarithmic(50.0))
+            //.with_value_to_string(formatters::v2s_f32_gain_to_db(2))
+            //.with_string_to_value(formatters::s2v_f32_gain_to_db()),
         }
     }
 }
@@ -204,14 +119,10 @@ impl Default for LiveSamplerParams {
 impl Default for LiveSampler {
     fn default() -> Self {
         Self {
+            channels: Channels::new(0),
             audio_io_layout: AudioIOLayout::default(),
             params: Arc::new(LiveSamplerParams::default()),
             sample_rate: -1.0,
-            buf: Bufs::default(),
-            volume: PolyVolumeEnv::new(Vec::new()),
-            passthru_volume: PolyVolumeEnv::new(Vec::new()),
-            state: State::default(),
-            now: Vec::new(),
         }
     }
 }
@@ -226,88 +137,6 @@ impl LiveSampler {
             .try_into()
             .unwrap();
         channel_count
-    }
-    fn toggle_passthru(&mut self, new_passthru: bool) {
-        if new_passthru != self.state.passthru {
-            let mut passthru = &mut self.state.passthru;
-            *passthru = new_passthru;
-        }
-    }
-    fn toggle_reverse(&mut self, new_reverse: bool) {
-        if let Some(Playing { speed, .. }) = &mut self.state.playing {
-            let old_reverse = *speed < 0.0;
-            if new_reverse != old_reverse {
-                *speed = -1.0 * *speed;
-            }
-        }
-    }
-
-    fn log_prefix(&self) -> String {
-        let seconds = format!("{:.3}s", (self.now[0] as f32) / self.sample_rate);
-        format!(
-            "{:>5} {:>5} {:>5}",
-            seconds,
-            self.now[0],
-            self.volume.value(0, self.now[0])
-        )
-    }
-
-    fn start_playing(&mut self, pos: f32, speed: f32) {
-        if self.state.playing.is_none() {
-            self.buf.seek(pos);
-            self.state.playing = Some(Playing { speed });
-            self.fade(1.0, None);
-            self.fade_passthru(0.0, None);
-        }
-    }
-
-    fn passthru_active(&self) -> bool {
-        self.state.passthru || self.params.passthru.value()
-    }
-
-    fn start_recording(&mut self) {
-        if !self.state.recording {
-            nih_warn!("{} start playing", self.log_prefix());
-            #[cfg(debug_assertions)]
-            self.buf.check_write_positions_at_zero();
-            self.state.recording = true;
-        }
-    }
-
-    fn stop_recording(&mut self) {
-        if self.state.recording {
-            // Vec::drain
-            self.buf.truncate_written();
-            self.state.recording = false;
-        }
-    }
-
-    fn fade_passthru(&mut self, target: f32, time: Option<usize>) {
-        self.passthru_volume.retrigger(
-            &self.now,
-            &self.now,
-            time.unwrap_or_else(|| self.params.fade_time.value() as usize),
-            target,
-        );
-    }
-
-    fn fade(&mut self, target: f32, time: Option<usize>) {
-        self.volume.retrigger(
-            &self.now,
-            &self.now,
-            time.unwrap_or_else(|| self.params.fade_time.value() as usize),
-            target,
-        );
-    }
-
-    fn stop_playing(&mut self) {
-        if self.state.playing.is_some() {
-            nih_warn!("{} stop playing", self.log_prefix());
-            self.state.playing = None;
-            // FIXME: Clicks. Cut at zero crossing. Try lookahead within buffer with better logging again. Try half-note mode then.
-            self.fade(0.0, None);
-            self.fade_passthru(1.0, None);
-        }
     }
 }
 
@@ -355,23 +184,15 @@ impl Plugin for LiveSampler {
         context: &mut impl InitContext<Self>,
     ) -> bool {
         self.audio_io_layout = audio_io_layout.clone();
+        self.channels = Channels::new(self.channel_count());
         self.sample_rate = buffer_config.sample_rate;
         let channel_count: usize = self.channel_count();
-        self.volume = PolyVolumeEnv::new(vec![0.0; channel_count]);
-        self.passthru_volume = PolyVolumeEnv::new(vec![1.0; channel_count]);
-        self.buf = Bufs::new(channel_count);
-        self.now = vec![0; channel_count];
-        nih_warn!("{} initialize", self.log_prefix());
         true
     }
 
     fn reset(&mut self) {
         let channel_count: usize = self.channel_count();
-        self.buf.clear();
-        self.state = State::default();
-        self.volume = PolyVolumeEnv::new(vec![0.0; channel_count]);
-        self.passthru_volume = PolyVolumeEnv::new(vec![1.0; channel_count]);
-        nih_warn!("{} reset", self.log_prefix());
+        self.channels = Channels::new(self.channel_count());
     }
 
     fn process(
@@ -382,12 +203,7 @@ impl Plugin for LiveSampler {
     ) -> ProcessStatus {
         let channels = buffer.channels();
         let mut next_event = context.next_event();
-        let prev_state = self.state.clone();
 
-        let params_speed = self.params.speed.smoothed.next();
-        let params_gain = self.params.gain.smoothed.next();
-        let params_fade_time = self.params.fade_time.smoothed.next() as usize;
-        let num_samples = buffer.samples();
         for (sample_id, mut channel_samples) in buffer.iter_samples().enumerate() {
             // Smoothing is optionally built into the parameters themselves
             let gain = self.params.gain.smoothed.next();
@@ -399,66 +215,101 @@ impl Plugin for LiveSampler {
                 }
                 //nih_warn!("USE sample_id={} event={:?}", sample_id, event);
                 nih_warn!("sample_id={} event={:?}", sample_id, event);
+                nih_warn!(
+                    "stuff r={} w={} p={} rev={} pass={} play={}",
+                    read = self.channels.channels[0].read,
+                    write = self.channels.channels[0].write,
+                    sample_pos = self.channels.channels[0].calc_sample_pos(),
+                    reverse = self.channels.channels[0].reverse_playback,
+                    passthru = self.channels.channels[0].passthru_volume,
+                    playback = self.channels.channels[0].playback_volume
+                );
                 match event {
                     NoteEvent::NoteOn { note, .. } => match note {
                         47 => {
-                            self.toggle_passthru(true);
+                            self.channels.each(|ch| ch.passthru_volume = 1.0);
                         }
                         48 => {
-                            self.start_recording();
+                            self.channels.each(|ch| {
+                                if !ch.recording {
+                                    ch.write = 0;
+                                    ch.recording = true;
+                                }
+                            });
                         }
                         49 => {
-                            self.toggle_reverse(true);
+                            self.channels.each(|ch| ch.reverse_playback = true);
                         }
                         60..=75 => {
                             let pos = (note - 60) as f32 / 16.0;
-                            self.start_playing(pos, 1.0);
+                            self.channels.each(|ch| {
+                                ch.read = ((ch.data.len() as f32) * pos);
+                                ch.playback_volume = 1.0;
+                                ch.passthru_volume = 0.0;
+                            });
                         }
                         _ => (),
                     },
                     NoteEvent::NoteOff { note, .. } => match note {
                         47 => {
-                            self.toggle_passthru(false);
+                            self.channels.each(|ch| ch.passthru_volume = 0.0);
                         }
                         48 => {
-                            self.stop_recording();
+                            self.channels.each(|ch| {
+                                ch.recording = false;
+                                ch.data.truncate(ch.write);
+                                ch.write = 0;
+                            });
                         }
                         49 => {
-                            self.toggle_reverse(false);
+                            self.channels.each(|ch| ch.reverse_playback = false);
                         }
                         60..=75 => {
-                            self.stop_playing();
+                            self.channels.each(|ch| {
+                                ch.playback_volume = 0.0;
+                                ch.passthru_volume = 1.0;
+                            });
                         }
                         _ => (),
                     },
-                    _ => {
-                        // nih_warn!("ignore event {:?}", event);
-                    }
+                    _ => (),
                 }
                 next_event = context.next_event();
             }
 
+            let params_speed = self.params.speed.smoothed.next();
+            let params_gain = self.params.gain.smoothed.next();
+            let params_passthru = if self.params.passthru.value() {
+                1.0
+            } else {
+                0.0
+            };
             for (channel, sample) in channel_samples.iter_mut().enumerate() {
                 let value = *sample;
-                if self.state.recording {
-                    self.buf.write(channel, value);
-                }
-                let new_sample = if let Some(Playing { speed }) = &mut self.state.playing {
-                    let value = self.buf.read(channel, *speed * params_speed);
-                    let volume = self.volume.value(channel, self.now[channel]);
-                    // Maybe
-                    let value = value * volume;
-                    value
-                } else {
-                    if self.passthru_active() {
-                        let volume = self.passthru_volume.value(channel, self.now[channel]);
-                        value * volume
+                let mut ch = &mut self.channels.channels[channel];
+
+                if ch.recording {
+                    if ch.write >= ch.data.len() {
+                        ch.data.push(value);
                     } else {
-                        0.0
+                        ch.data[ch.write] = value;
                     }
+                    ch.write += 1;
+                }
+
+                let mut output = 0.0;
+                output += value * ch.passthru_volume * params_passthru;
+                output += if !ch.data.is_empty() {
+                    ch.playback_volume * ch.data[ch.calc_sample_pos()]
+                } else {
+                    0.0
                 };
-                *sample = new_sample * gain;
-                self.now[channel] += 1;
+
+                let direction = if ch.reverse_playback { -1.0f32 } else { 1.0f32 };
+                ch.read += params_speed * direction;
+                output *= params_gain;
+
+                *sample = output;
             }
         }
 
