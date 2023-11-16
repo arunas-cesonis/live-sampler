1935ca7663afdc7c2dacb0f7b6cd61646d1afe80
Arunas <karunce@gmail.com>
2023-11-16 20:47:15 +0000

    Move live sampler to workspace

--- audio-sampler/src/lib.rs
+++ audio-sampler/src/lib.rs
@@ -1,4 +1,7 @@
-use std::sync::Arc;
+use std::fmt;
+use std::io::Write;
+use std::sync::{Arc, Mutex};
+use std::time::UNIX_EPOCH;
 
 use nih_plug::prelude::*;
 
@@ -7,11 +10,27 @@ use crate::sampler::Sampler;
 mod sampler;
 mod volume;
 
+type SysEx = ();
+
+#[derive(Clone, Debug)]
+struct RecordedEvent {
+    original: NoteEvent<SysEx>,
+    offset: isize,
+}
+
+#[derive(Clone, Debug, Default)]
+struct EventSampler {
+    data: Vec<RecordedEvent>,
+    recording_offset: isize,
+    recording: bool,
+}
+
 pub struct LiveSampler {
     audio_io_layout: AudioIOLayout,
     params: Arc<LiveSamplerParams>,
     sample_rate: f32,
     sampler: Sampler,
+    debug: Arc<Mutex<Option<std::fs::File>>>,
 }
 
 #[derive(Params)]
@@ -56,6 +75,7 @@ impl Default for LiveSampler {
             params: Arc::new(LiveSamplerParams::default()),
             sample_rate: -1.0,
             sampler: Sampler::new(0, &sampler::Params::default()),
+            debug: Arc::new(Mutex::new(None)),
         }
     }
 }
@@ -70,6 +90,14 @@ impl LiveSampler {
             .try_into()
             .unwrap();
         channel_count
+    }
+    fn debug_println(&mut self, fmt: fmt::Arguments) {
+        let f = self.debug.lock();
+        let binding = f.unwrap();
+        let mut file = binding.as_ref().unwrap();
+        file.write_fmt(fmt).unwrap();
+        file.write(&[b'\n']).unwrap();
+        file.flush().unwrap();
     }
     fn sampler_params(&self) -> sampler::Params {
         let params_speed = self.params.speed.smoothed.next();
@@ -108,7 +136,7 @@ impl Plugin for LiveSampler {
     ];
     const MIDI_INPUT: MidiConfig = MidiConfig::MidiCCs;
     const SAMPLE_ACCURATE_AUTOMATION: bool = true;
-    type SysExMessage = ();
+    type SysExMessage = SysEx;
 
     type BackgroundTask = ();
 
@@ -125,6 +153,17 @@ impl Plugin for LiveSampler {
         self.audio_io_layout = audio_io_layout.clone();
         self.sample_rate = buffer_config.sample_rate;
         self.sampler = Sampler::new(self.channel_count(), &self.sampler_params());
+        let debug = std::fs::File::create(format!(
+            "/tmp/live-sampler-{}-{}.log",
+            std::process::id(),
+            std::time::SystemTime::now()
+                .duration_since(UNIX_EPOCH)
+                .unwrap()
+                .as_millis()
+        ))
+        .unwrap();
+        let mut f = self.debug.lock().unwrap();
+        *f = Some(debug);
         true
     }
 
@@ -147,6 +186,7 @@ impl Plugin for LiveSampler {
                 if event.timing() != sample_id as u32 {
                     break;
                 }
+                self.debug_println(format_args!("{:?}", event));
                 //nih_warn!("event {:?}", event);
                 // assert!(event.voice_id().is_none());
                 match event {
