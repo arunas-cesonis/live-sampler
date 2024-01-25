38bd259cc285616f033566360831b2e71a229986
Arunas <karunce@gmail.com>
2024-01-25 17:09:32 +0000

    Fix loop mode

--- audio_sampler/src/clip.rs
+++ audio_sampler/src/clip.rs
@@ -1,242 +1,236 @@
-use crate::sampler::LoopMode;
 use smallvec::SmallVec;
 use std::cmp::Ordering;
-use std::fmt::Debug;
-use std::fs::read;
 
-pub trait F:
-    std::ops::Rem<Output = Self>
-    + std::ops::Add<Output = Self>
-    + std::ops::Sub<Output = Self>
-    + PartialOrd
-    + std::ops::Neg<Output = Self>
-    + num_traits::Zero
-    + num_traits::Signed
-    + std::iter::Sum
-    + Default
-    + Copy
-{
+#[derive(Default, Debug, Clone, Copy)]
+pub struct Position {
+    index: usize,
+    offset: f32,
+    direction: f32,
 }
 
-#[derive(Default, Copy, Clone, Debug)]
-pub struct Interval<T> {
-    start: T,
-    end: T,
+impl Position {
+    pub fn start(v: &[Interval]) -> Self {
+        Position {
+            index: 0,
+            offset: v[0].start,
+            direction: 1.0,
+        }
+    }
+
+    fn get_valid_index_offset(&self, v: &[Interval]) -> (usize, f32) {
+        if self.index < v.len() && v[self.index].contains(self.offset) {
+            (self.index, self.offset)
+        } else {
+            if let Some(index) = v.iter().position(|x| x.contains(self.offset)) {
+                (index, self.offset)
+            } else {
+                (0, v[0].start)
+            }
+        }
+    }
+
+    pub fn get_valid(&self, v: &[Interval]) -> Self {
+        let (mut index, mut offset) = self.get_valid_index_offset(v);
+        Self {
+            index,
+            offset,
+            direction: self.direction,
+        }
+    }
+
+    pub fn to_data_index(&self, v: &[Interval], speed: f32, data_len: usize) -> usize {
+        let offset = if self.direction * speed >= 0.0 {
+            self.offset
+        } else {
+            self.advance(&v, -1.0).offset
+        };
+        (offset.round() as usize) % data_len
+    }
+
+    pub fn advance(&self, v: &[Interval], mut amount: f32) -> Self {
+        let (mut index, mut offset) = self.get_valid_index_offset(v);
+        while amount < 0.0 {
+            assert!(v[index].contains(offset) || v[index].at_the_end(offset));
+            let rem = offset - v[index].start;
+            if rem == 0.0 {
+                index = (index + v.len() - 1) % v.len();
+                offset = v[index].end;
+                continue;
+            }
+            if rem > -amount {
+                offset += amount;
+                amount = 0.0;
+            } else {
+                amount += rem;
+                index = (index + v.len() - 1) % v.len();
+                offset = v[index].end;
+            }
+        }
+        while amount > 0.0 {
+            assert!(v[index].contains(offset));
+            let rem = v[index].end - offset;
+            if rem > amount {
+                offset += amount;
+                amount = 0.0;
+            } else {
+                amount -= rem;
+                index = (index + 1) % v.len();
+                offset = v[index].start;
+            }
+        }
+        if v[index].at_the_end(offset) {
+            index = (index + 1) % v.len();
+            offset = v[index].start;
+        }
+        Position {
+            index,
+            offset,
+            direction: self.direction,
+        }
+    }
 }
 
-impl<T> Interval<T>
-where
-    T: std::ops::Rem<Output = T>
-        + std::ops::Add<Output = T>
-        + std::ops::Sub<Output = T>
-        + PartialOrd
-        + std::ops::Neg<Output = T>
-        + num_traits::Zero
-        + num_traits::Signed
-        + std::iter::Sum
-        + Default
-        + Copy,
-{
-    pub fn new(start: T, end: T) -> Self {
-        assert!(start < end);
-        Self { start, end }
-    }
+#[derive(Default, Debug, Clone, Copy)]
+pub struct Interval {
+    start: f32,
+    end: f32,
+}
 
-    pub fn duration(&self) -> T {
-        self.end - self.start
-    }
-
-    pub fn contains(&self, offset: T) -> bool {
+impl Interval {
+    pub fn contains(&self, offset: f32) -> bool {
         self.start <= offset && offset < self.end
     }
 
-    pub fn at_the_end(&self, offset: T) -> bool {
+    pub fn at_the_end(&self, offset: f32) -> bool {
         offset == self.end
     }
 
-    pub fn distance(&self, offset: T) -> T {
-        let start = (self.start - offset).abs();
-        let end = (self.end - offset).abs();
-        if start > end {
-            start
-        } else {
-            end
-        }
+    pub fn duration(&self) -> f32 {
+        self.end - self.start
     }
 }
 
-#[derive(Default, Clone, Debug)]
-pub struct Intervals<T> {
-    intervals: SmallVec<[Interval<T>; 4]>,
+#[derive(Default, Debug, Clone)]
+pub struct Intervals {
+    v: SmallVec<[Interval; 4]>,
 }
 
-impl<T> Intervals<T>
-where
-    T: std::ops::Rem<Output = T>
-        + std::ops::Add<Output = T>
-        + std::ops::Sub<Output = T>
-        + PartialOrd
-        + std::ops::Neg<Output = T>
-        + num_traits::Zero
-        + num_traits::Signed
-        + std::iter::Sum
-        + Default
-        + Debug
-        + Copy,
-{
-    pub fn duration(&self) -> T {
-        self.intervals.iter().map(|v| v.duration()).sum()
+impl Intervals {
+    pub fn duration(&self) -> f32 {
+        self.v.iter().map(|x| x.duration()).sum()
     }
 
-    pub fn position_from_start(&self, offset: T) -> Option<Position<T>> {
-        let mut pos = Position::start(self);
-        pos.advance(self, offset);
-        Some(pos)
+    pub fn push(&mut self, start: f32, end: f32) {
+        assert!(start < end);
+        self.v.push(Interval { start, end })
     }
 
-    pub fn push(&mut self, start: T, end: T) {
-        self.intervals.push(Interval::new(start, end));
+    pub fn start(&self) -> f32 {
+        self.v[0].start
     }
 
-    pub fn to_vec(&self) -> Vec<(T, T)> {
-        self.intervals.iter().map(|x| (x.start, x.end)).collect()
+    pub fn contains(&self, offset: f32) -> bool {
+        self.v.iter().any(|x| x.contains(offset))
     }
 
-    pub fn next_index(&self, index: usize) -> usize {
-        assert!(!self.intervals.is_empty());
-        if index + 1 < self.intervals.len() {
-            index + 1
-        } else {
-            0
+    pub fn wrapped_global(&self, x: f32) -> Option<f32> {
+        let d = self.duration();
+        let x = x % d;
+        let x = if x < 0.0 { x + d } else { x };
+
+        self.global(x)
+    }
+
+    pub fn first_local(&self, x: f32) -> Option<f32> {
+        assert!(x >= 0.0);
+        let mut offset = 0.0;
+        for interval in &self.v {
+            let s = interval.start;
+            let e = interval.end;
+            let d = e - s;
+            if s <= x && x < e {
+                return Some(offset + (x - s));
+            }
+            offset += d;
         }
+        None
     }
 
-    pub fn prev_index(&self, index: usize) -> usize {
-        assert!(!self.intervals.is_empty());
-        if index > 0 {
-            index - 1
-        } else {
-            self.intervals.len() - 1
+    pub fn local(&self, x: f32) -> Vec<f32> {
+        assert!(x >= 0.0);
+        let mut offset = 0.0;
+        let mut result = vec![];
+        for interval in &self.v {
+            let s = interval.start;
+            let e = interval.end;
+            let d = e - s;
+            if s <= x && x < e {
+                result.push(offset + (x - s));
+            }
+            offset += d;
         }
+        result
     }
 
-    pub fn nearest_index(&self, offset: T) -> Option<usize> {
-        self.intervals
-            .iter()
-            .enumerate()
-            .min_by(|(i, x), (j, y)| {
-                let a = x.distance(offset);
-                let b = y.distance(offset);
-                if a < b {
-                    Ordering::Less
-                } else if a > b {
-                    Ordering::Greater
-                } else {
-                    Ordering::Equal
-                }
-            })
-            .map(|x| x.0)
-    }
-
-    pub fn first_index_contains(&self, offset: T) -> Option<usize> {
-        self.intervals.iter().position(|x| x.contains(offset))
-    }
-}
-
-#[derive(Default, Clone, Copy, Debug)]
-pub struct Position<T> {
-    index: usize,
-    offset: T,
-}
-
-impl<T> Position<T>
-where
-    T: std::ops::Rem<Output = T>
-        + std::ops::Add<Output = T>
-        + std::ops::Sub<Output = T>
-        + PartialOrd
-        + std::ops::Neg<Output = T>
-        + num_traits::Zero
-        + num_traits::Signed
-        + std::iter::Sum
-        + Default
-        + std::fmt::Debug
-        + Copy,
-{
-    pub fn start(v: &Intervals<T>) -> Self {
-        Self {
-            index: 0,
-            offset: v.intervals[0].start,
+    pub fn global(&self, x: f32) -> Option<f32> {
+        assert!(x >= 0.0);
+        let mut offset = 0.0;
+        for interval in &self.v {
+            let s = interval.start;
+            let e = interval.end;
+            let d = e - s;
+            if offset <= x && x < offset + d {
+                return Some(s + (x - offset));
+            }
+            offset += d;
         }
+        None
     }
 
-    pub fn ensure_within(&mut self, v: &Intervals<T>) {
-        let (index, offset) =
-            if self.index < v.intervals.len() && v.intervals[self.index].contains(self.offset) {
-                (self.index, self.offset)
-            } else {
-                if let Some(index) = v.first_index_contains(self.offset) {
-                    (index, self.offset)
-                } else {
-                    let index = v.nearest_index(self.offset);
-                    (index, v.intervals[index].start)
-                }
-            };
-        self.index = index;
-        self.offset = offset;
-    }
-
-    pub fn advance(&mut self, v: &Intervals<T>, mut amount: T) {
-        self.ensure_within(v);
-        let mut index = self.index;
-        let mut offset = self.offset;
-
-        while amount < T::zero() {
-            assert!(v.intervals[index].contains(offset) || v.intervals[index].at_the_end(offset));
-            if offset == v.intervals[index].start {
-                index = v.prev_index(index);
-                offset = v.intervals[index].end;
-                continue;
-            };
-            let remaining = offset - v.intervals[index].start;
-            if remaining > amount.abs() {
-                offset = offset + amount;
-                amount = T::zero();
-            } else if remaining < amount.abs() {
-                index = v.prev_index(index);
-                offset = v.intervals[index].end;
-                amount = amount + remaining;
-            } else {
-                offset = v.intervals[index].start;
-                amount = T::zero();
+    // questionable
+    pub fn nearest_global(&self, x: f32) -> Option<f32> {
+        let (mut min, mut min_d) = match self.v.first() {
+            Some(interval) => (interval.start, (x - interval.start).abs()),
+            None => return None,
+        };
+        for i in 0..self.v.len() {
+            let s = self.v[i].start;
+            let e = self.v[i].end;
+            let ds = (x - s).abs();
+            let de = (x - e).abs();
+            if ds < min_d {
+                min_d = ds;
+                min = s;
+            }
+            if de < min_d {
+                min_d = de;
+                min = self.v[(i + 1) % self.v.len()].start;
             }
         }
+        Some(min)
+    }
 
-        while amount > T::zero() {
-            assert!(v.intervals[index].contains(offset));
-            let remaining = v.intervals[index].end - offset;
-            if remaining <= amount {
-                index = v.next_index(index);
-                offset = v.intervals[index].start;
-                amount = amount - remaining;
-            } else {
-                offset = offset + amount;
-                amount = T::zero();
-            }
-        }
-        self.index = index;
-        self.offset = offset;
+    pub fn as_slice(&self) -> &[Interval] {
+        self.v.as_slice()
     }
 }
 
 #[cfg(test)]
 mod test {
+
     use super::*;
 
     #[test]
-    fn test_position() {
-        let mut v = Intervals::<f32>::default();
-        let pos = v.position_from_start(10.0);
-        eprintln!("{:?}", pos);
+    fn test_intervals() {
+        let mut view = Intervals::default();
+        view.push(10.0, 20.0);
+        let pos = Position::start(view.as_slice());
+
+        //view.push(100.0, 190.0);
+        let mut pos = pos.advance(view.as_slice(), 5.0);
+        for _ in 0..100 {
+            pos = pos.advance(view.as_slice(), -1.3);
+            eprintln!("{:?}", pos);
+        }
     }
 }
