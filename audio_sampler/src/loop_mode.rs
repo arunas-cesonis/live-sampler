90328514ad9004ade3f3eb6aaf8c18c3905900b5
Arunas <karunce@gmail.com>
2024-01-29 20:02:12 +0000

    Not good

--- audio_sampler/src/loop_mode.rs
+++ audio_sampler/src/loop_mode.rs
@@ -6,3 +6,18 @@ pub enum LoopMode {
     PingPong,
     Loop,
 }
+
+#[derive(Debug, Enum, PartialEq, Clone, Copy)]
+pub enum LoopModeParam {
+    PlayOnce,
+    Loop,
+}
+
+impl LoopMode {
+    pub fn from_param(param: LoopModeParam) -> Self {
+        match param {
+            LoopModeParam::PlayOnce => LoopMode::PlayOnce,
+            LoopModeParam::Loop => LoopMode::Loop,
+        }
+    }
+}
