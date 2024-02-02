19b50bbf30e7b526ca9bbd0c6604cb4b52727125
Arunas <karunce@gmail.com>
2024-02-02 12:23:52 +0000

    Remove more

--- audio_sampler/src/utils.rs
+++ audio_sampler/src/utils.rs
@@ -80,66 +80,6 @@ where
     x.abs()
 }
 
-#[cfg(test)]
-mod test_loop {
-    use super::*;
-
-    #[test]
-    fn test_bound() {
-        let data_len = Bound::new(100.0);
-        let start = Offset::new(90.0);
-        let length = Bound::new(20.0);
-        let multiply = Multiply::new(0.5);
-        for i in 0..100 {
-            let i = i as f32;
-            let x = data_len.apply(start.apply(length.apply(multiply.apply(i))));
-            eprintln!("i={} x={}", i, x);
-        }
-    }
-}
-
-pub struct Bound {
-    bound: f32,
-}
-
-impl Bound {
-    pub fn new(bound: f32) -> Self {
-        Self { bound }
-    }
-
-    pub fn apply(&self, x: f32) -> f32 {
-        normalize_offset(x, self.bound)
-    }
-}
-
-pub struct Multiply {
-    value: f32,
-}
-
-impl Multiply {
-    pub fn new(value: f32) -> Self {
-        Self { value }
-    }
-
-    pub fn apply(&self, x: f32) -> f32 {
-        x * self.value
-    }
-}
-
-pub struct Offset {
-    amount: f32,
-}
-
-impl Offset {
-    pub fn new(amount: f32) -> Self {
-        Self { amount }
-    }
-
-    pub fn apply(&self, x: f32) -> f32 {
-        x + self.amount
-    }
-}
-
 // https://github.com/robbert-vdh/nih-plug/blob/92ce73700005255565c6be45412609ea87eb8b41/src/util.rs#L38
 pub const MINUS_INFINITY_GAIN: f32 = 1e-5; // 10f32.powf(MINUS_INFINITY_DB / 20)
 
