e8fa7b063c04c92c84d507a8f4dbaa72fabf4acb
Arunas <karunce@gmail.com>
2024-02-02 12:12:41 +0000

    Cleanup

--- audio_sampler/src/utils.rs
+++ audio_sampler/src/utils.rs
@@ -142,3 +142,14 @@ impl Offset {
         x + self.amount
     }
 }
+
+// https://github.com/robbert-vdh/nih-plug/blob/92ce73700005255565c6be45412609ea87eb8b41/src/util.rs#L38
+pub const MINUS_INFINITY_GAIN: f32 = 1e-5; // 10f32.powf(MINUS_INFINITY_DB / 20)
+
+/// Convert a voltage gain ratio to decibels. Gain ratios that aren't positive will be treated as
+///
+/// [`MINUS_INFINITY_DB`].
+#[inline]
+pub fn gain_to_db(gain: f32) -> f32 {
+    f32::max(gain, MINUS_INFINITY_GAIN).log10() * 20.0
+}
