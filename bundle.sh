40ea545d2695f67df87c74cd7b26364c50742649
Arunas <karunce@gmail.com>
2024-02-06 18:11:34 +0000

    Fix bunlde

--- bundle.sh
+++ bundle.sh
@@ -4,6 +4,7 @@ echo "
 # Ensure nih-plug is installed:
 # cargo install --git https://github.com/robbert-vdh/nih-plug.git cargo-nih-plug
 "
+#cargo nih-plug bundle audio_sampler $@
 cargo nih-plug bundle audio_sampler $@
 # cargo nih-plug bundle -p midi_sampler -p audio_sampler $@
 #cargo nih-plug bundle midi-sampler $@
