5d26e8ca9626b3038119b4239db6c978c9597948
Arunas <karunce@gmail.com>
2024-02-16 22:51:28 +0000

    Bare minimum

--- pyo3-plugin/src/lib.rs
+++ pyo3-plugin/src/lib.rs
@@ -1,3 +1,5 @@
+extern crate core;
+
 mod common_types;
 mod editor_vizia;
 
@@ -7,9 +9,11 @@ use std::path::PathBuf;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::Arc;
 
-use crate::common_types::{FileStatus, Status};
+use crate::common_types::{EvalError, EvalStatus, FileStatus, Status};
 use nih_plug::prelude::*;
 use nih_plug_vizia::ViziaState;
+use pyo3::types::{IntoPyDict, PyList};
+use pyo3::{PyErr, Python};
 
 type SysEx = ();
 
@@ -20,6 +24,8 @@ pub struct PyO3Plugin {
     status_in: Arc<parking_lot::Mutex<triple_buffer::Input<Status>>>,
     status_out: Arc<parking_lot::Mutex<triple_buffer::Output<Status>>>,
     seen_data_version: usize,
+    python_source: Option<String>,
+    status: Status,
 }
 
 #[derive(Default)]
@@ -39,10 +45,6 @@ impl<'a> PersistentField<'a, String> for SourcePath {
 
 #[derive(Params)]
 pub struct PyO3PluginParams {
-    //    #[id = "speed"]
-    //    pub speed: FloatParam,
-    //    #[id = "fade time"]
-    //    pub fade_time: FloatParam,
     #[persist = "source-path"]
     source_path: SourcePath,
     #[persist = "editor-state"]
@@ -53,7 +55,11 @@ impl Default for PyO3PluginParams {
     fn default() -> Self {
         Self {
             editor_state: editor_vizia::default_state(),
-            source_path: SourcePath::default(),
+            #[cfg(debug_assertions)]
+            source_path: SourcePath(Arc::new(parking_lot::Mutex::new("./a.py".to_string()))),
+            #[cfg(not(debug_assertions))]
+            source_path: SourcePath::default(), //#[cfg(not(debug_assertions))]
+                                                //            source_path: SourcePath::default(),
         }
     }
 }
@@ -66,14 +72,124 @@ impl Default for PyO3Plugin {
             sample_rate: 0.0,
             status_in: Arc::new(parking_lot::Mutex::new(status_in)),
             status_out: Arc::new(parking_lot::Mutex::new(status_out)),
-
-            data_version: Arc::new(AtomicUsize::new(0)),
+            data_version: Arc::new(AtomicUsize::new(1)),
             seen_data_version: 0,
+            python_source: None,
+            status: Status::default(),
         }
     }
 }
 
-impl PyO3Plugin {}
+use pyo3::prelude::*;
+
+#[pyfunction]
+#[pyo3(name = "print")]
+fn python_print(a: &PyAny) {
+    nih_warn!("python: {:?}", a);
+}
+
+#[pymodule]
+fn module_with_functions(py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(python_print, m)?)?;
+    Ok(())
+}
+
+impl PyO3Plugin {
+    fn update_file_status(&mut self, file_status: FileStatus) {
+        self.status.file_status = file_status;
+        self.status_in.lock().write(self.status.clone());
+    }
+
+    fn update_eval_status(&mut self, eval_status: EvalStatus) {
+        self.status.eval_status = eval_status;
+        self.status_in.lock().write(self.status.clone());
+    }
+
+    fn update_python_source(&mut self) {
+        let data_version = self.data_version.load(Ordering::Relaxed);
+        if data_version != self.seen_data_version {
+            self.seen_data_version = data_version;
+            let path = { self.params.source_path.0.lock().clone() };
+            nih_warn!("data_version={:?} path={:?}", data_version, path);
+            if path == "" {
+                self.update_file_status(FileStatus::Unloaded);
+                return;
+            }
+            let ret = std::fs::read_to_string(&*path);
+            match ret {
+                Ok(source) => {
+                    let size = source.len();
+                    self.python_source = Some(source);
+                    nih_log!("python source: {}", self.python_source.as_ref().unwrap());
+                    self.update_file_status(FileStatus::Loaded(
+                        path.to_string(),
+                        size.try_into().unwrap(),
+                    ));
+                }
+                Err(e) => {
+                    nih_log!("python not laoded: {}", e);
+                    self.update_file_status(FileStatus::Error(e.to_string()));
+                }
+            }
+        }
+    }
+
+    fn copyback_buffer(&self, buf: &mut Buffer, result: Vec<Vec<f32>>) -> Result<(), EvalError> {
+        let nc = buf.channels();
+        let ns = buf.samples();
+        if nc != result.len() {
+            return Err(EvalError::OtherError(format!(
+                "Number of channels returned from python ({}) does not match the buffer ({}):",
+                result.len(),
+                nc
+            )));
+        }
+        if let Some((i, xlen)) = result.iter().enumerate().find_map(|(i, x)| {
+            if x.len() != ns {
+                Some((i, x.len()))
+            } else {
+                None
+            }
+        }) {
+            return Err(EvalError::OtherError(format!(
+                "Number of samples returned from python ({}) does not match the number of samples in the buffer ({}) at channel {}",
+                xlen, ns, i
+            )));
+        }
+        let sl = buf.as_slice();
+        for i in 0..ns {
+            for j in 0..nc {
+                sl[j][i] = result[j][i];
+            }
+        }
+        Ok(())
+    }
+
+    fn run_python(&mut self, buffer: &mut Buffer) -> std::result::Result<(), EvalError> {
+        if let Some(python_source) = &self.python_source {
+            let buf = buffer.as_slice();
+            let result = Python::with_gil(|py| -> std::result::Result<Vec<Vec<f32>>, PyErr> {
+                //let m = py.import("module_with_functions")?;
+                //let globals = [("module_with_functions", m)].into_py_dict(py);
+                py.run(python_source.as_str(), None, None)?;
+                let tmp = buf.iter().map(|x| x.to_vec()).collect::<Vec<_>>();
+                let pybuf = PyList::new(py, tmp);
+                let locals = [("buffer", pybuf)].into_py_dict(py);
+
+                let result: Vec<Vec<f32>> =
+                    py.eval("process(buffer)", None, Some(locals))?.extract()?;
+                Ok(result)
+            });
+            self.copyback_buffer(
+                buffer,
+                result.map_err(|e| EvalError::PythonError(e.to_string()))?,
+            )?;
+        } else {
+            return Err(EvalError::OtherError("no source loaded".to_string()));
+        }
+        Ok(())
+    }
+}
 
 impl Plugin for PyO3Plugin {
     const NAME: &'static str = "PyO3Plugin";
@@ -104,8 +220,6 @@ impl Plugin for PyO3Plugin {
     type BackgroundTask = ();
 
     fn editor(&mut self, _async_executor: AsyncExecutor<Self>) -> Option<Box<dyn Editor>> {
-        // Using vizia as Iced doesn't support drawing bitmap images under OpenGL
-
         let data = editor_vizia::Data {
             version: self.data_version.clone(),
             params: self.params.clone(),
@@ -137,39 +251,20 @@ impl Plugin for PyO3Plugin {
         _aux: &mut AuxiliaryBuffers,
         context: &mut impl ProcessContext<Self>,
     ) -> ProcessStatus {
+        self.update_python_source();
+        let result = self.run_python(buffer);
+        match result {
+            Ok(()) => {
+                self.update_eval_status(EvalStatus::Ok);
+            }
+            Err(e) => {
+                self.update_eval_status(EvalStatus::Error(e));
+            }
+        };
+
         let mut next_event = context.next_event();
 
-        if let data_version = self.data_version.load(Ordering::Relaxed) {
-            if data_version != self.seen_data_version {
-                self.seen_data_version = data_version;
-                let path = self.params.source_path.0.lock();
-                let ret = std::fs::metadata(&*path);
-                match ret {
-                    Ok(metadata) => {
-                        let size = metadata.len();
-                        let status = Status {
-                            file_status: FileStatus::Loaded(
-                                path.to_string(),
-                                size.try_into().unwrap(),
-                            ),
-                        };
-                        self.status_in.lock().write(status);
-                    }
-                    Err(e) => {
-                        let status = Status {
-                            file_status: FileStatus::Error(e.to_string()),
-                        };
-                        self.status_in.lock().write(status);
-                    }
-                }
-                //self.sampler = self.sampler_params(context);
-            }
-        }
-
         for (sample_id, _channel_samples) in buffer.iter_samples().enumerate() {
-            //let params = self.sampler_params(context);
-            //let params = &params;
-
             let mut events = vec![];
             while let Some(event) = next_event {
                 if event.timing() != sample_id as u32 {
@@ -179,14 +274,9 @@ impl Plugin for PyO3Plugin {
                 next_event = context.next_event();
             }
 
-            // let events = self.sampler.process_sample(events, params);
             for e in events {
-                //let e = set_event_timing(e, sample_id as u32);
-                //nih_warn!("OUTPUT: {:?}", e);
                 context.send_event(e);
             }
-
-            //self.sampler.process_sample(channel_samples, params);
         }
 
         ProcessStatus::Normal
@@ -194,8 +284,8 @@ impl Plugin for PyO3Plugin {
 }
 
 impl ClapPlugin for PyO3Plugin {
-    const CLAP_ID: &'static str = "com.midisampler";
-    const CLAP_DESCRIPTION: Option<&'static str> = Some("MIDI sampler");
+    const CLAP_ID: &'static str = "com.pyo3plugin";
+    const CLAP_DESCRIPTION: Option<&'static str> = Some("PyO3Plugin");
     const CLAP_MANUAL_URL: Option<&'static str> = Some(Self::URL);
     const CLAP_SUPPORT_URL: Option<&'static str> = None;
     const CLAP_FEATURES: &'static [ClapFeature] = &[
@@ -207,7 +297,7 @@ impl ClapPlugin for PyO3Plugin {
 }
 
 impl Vst3Plugin for PyO3Plugin {
-    const VST3_CLASS_ID: [u8; 16] = *b"MidiSamplerPlugi";
+    const VST3_CLASS_ID: [u8; 16] = *b"PyO3Pluginnnnnnn";
     const VST3_SUBCATEGORIES: &'static [Vst3SubCategory] =
         &[Vst3SubCategory::Fx, Vst3SubCategory::Tools];
 }
