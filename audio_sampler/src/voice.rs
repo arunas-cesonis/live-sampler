9f0c200dc0782eb55f7be7962ab86e2acad23c18
Arunas <karunce@gmail.com>
2024-02-19 13:28:57 +0000

    Forward loop

--- audio_sampler/src/voice.rs
+++ audio_sampler/src/voice.rs
@@ -1,15 +1,23 @@
-use crate::common_types::Note;
+use crate::common_types::{LoopMode, Note};
 use crate::volume::Volume;
 
 #[repr(transparent)]
 #[derive(Clone, Debug, PartialEq)]
 pub struct VoiceId(pub usize);
 
+#[derive(Clone, Copy, Debug)]
+pub struct Player {
+    pub offset: f32,
+    pub length: f32,
+}
+
 #[derive(Clone, Debug)]
 pub struct Voice {
     pub note: Note,
     pub loop_start_percent: f32,
     pub played: f32,
+    pub player: Player,
+    pub player_updated: usize,
     pub volume: Volume,
     pub finished: bool,
     // this is only used by the UI to show loop points
