bb9ded3fc0305bb8830f53e26df040800bdd0249
Arunas <karunce@gmail.com>
2024-02-27 19:43:12 +0000

    Add optional mimalloc

--- run_jack.sh
+++ run_jack.sh
@@ -2,4 +2,6 @@
 set -eu
 cargo build --package audio_sampler
 export DYLD_LIBRARY_PATH='/opt/homebrew/lib/'
+export MallocStackLogging=1
+export DYLD_INSERT_LIBRARIES=/usr/lib/libgmalloc.dylib
 ./target/debug/audio-sampler --backend jack --midi-input 'VirtualMidi Port1'
