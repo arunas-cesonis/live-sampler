1b574261a0e9867f71dd4575784c19465a1f60c5
Arunas <karunce@gmail.com>
2024-02-28 21:43:48 +0000

    Add cbindgen

--- audio_sampler_lib/src/lib.rs
+++ audio_sampler_lib/src/lib.rs
@@ -1,9 +1,12 @@
-pub mod sampler;
-pub mod common_types;
-pub mod time_value;
+extern crate core;
+
+mod capi;
 mod clip2;
+pub mod common_types;
 mod recorder;
+pub mod sampler;
+mod test_sampler;
+pub mod time_value;
 mod utils;
 mod voice;
 mod volume;
-mod test_sampler;
