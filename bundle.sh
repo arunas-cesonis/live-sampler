362882c3e61f6f45a521c45422772433120b8218
Arunas <karunce@gmail.com>
2024-06-11 08:39:21 +0000

    Cleanup bundle.sh

--- bundle.sh
+++ bundle.sh
@@ -4,8 +4,4 @@ echo "
 # Ensure nih-plug is installed:
 "
 cargo install --git https://github.com/robbert-vdh/nih-plug.git cargo-nih-plug
-#cargo nih-plug bundle audio_sampler $@
 cargo nih-plug bundle audio_sampler $@
-# cargo nih-plug bundle -p midi_sampler -p audio_sampler $@
-#cargo nih-plug bundle midi-sampler $@
-#cargo nih-plug bundle lua-plugin $@
